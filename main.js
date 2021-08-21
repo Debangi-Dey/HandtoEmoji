000182e5891f3510f1257a518a0d299a0510a53e
Debangi-Dey <74201092+Debangi-Dey@users.noreply.github.com>
2021-08-21 15:34:54 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -20,7 +20,6 @@ classifier=ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/J
 
 function model_loaded(){
     console.log('model is loaded');
-    speak()
 }
 
 function speak(){
@@ -29,4 +28,53 @@ function speak(){
     speak2="the second prediction is "+predict2;
     utter=new SpeechSynthesisUtterance(speak1+speak2);
     s.speak(utter);
+}
+
+function check() {
+    img=document.getElementById("capture_image");
+    classifier.classify(img, gotResult);
+}
+
+function gotResult(error, results){
+    if (error){
+        console.error(error);
+    }
+    else {
+        console.log(results);
+        document.getElementById("resultEmo").innerHTML=results[0].label;
+        document.getElementById("resultEmo2").innerHTML=results[1].label;
+        predict1=results[0].label;
+        predict2=results[1].label;
+        speak();
+        if (results[0].label=="Peace"){
+            document.getElementById("emoji1").innerHTML="&#9996;"
+        }
+        if (results[0].label=="Hand Raise"){
+            document.getElementById("emoji1").innerHTML="&#9995"
+        }
+        if (results[0].label=="Rock"){
+            document.getElementById("emoji1").innerHTML="&#129311;"
+        }
+        if (results[0].label=="Fist"){
+            document.getElementById("emoji1").innerHTML="&#128074;"
+        }
+        if (results[0].label=="Thumbs Up"){
+            document.getElementById("emoji1").innerHTML="&#128077"
+        }
+        if (results[1].label=="Peace"){
+            document.getElementById("emoji2").innerHTML="&#9996;"
+        }
+        if (results[1].label=="Hand Raise"){
+            document.getElementById("emoji2").innerHTML="&#9995"
+        }
+        if (results[1].label=="Rock"){
+            document.getElementById("emoji2").innerHTML="&#129311;"
+        }
+        if (results[1].label=="Fist"){
+            document.getElementById("emoji2").innerHTML="&#128074;"
+        }
+        if (results[1].label=="Thumbs Up"){
+            document.getElementById("emoji2").innerHTML="&#128077"
+        }
+    }
 }
